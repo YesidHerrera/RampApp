901a15b1787210642504f0da6cc4cce47d4c2442
yesid.herrera <yesid.herrera@wabtec.com>
2023-02-20 18:26:02 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ConnectionService, ConnectionState } from 'ng-connection-service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'RampApp';
+  status!: string;
+  currentState!: ConnectionState;
+  subscription = new Subscription();
+
+  constructor(private connectionService: ConnectionService){}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.connectionService.monitor().pipe(
+        tap((newState: ConnectionState) => {
+          this.currentState = newState;
+
+          if (this.currentState.hasNetworkConnection) {
+            this.status = 'ONLINE';
+          } else {
+            this.status = 'OFFLINE';
+          }
+        })
+      ).subscribe()
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
 }
